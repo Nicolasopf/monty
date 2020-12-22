ffa827e2a6cf493762b3379197c05678ad3d089c
Nicolas <nico15935746@gmail.com>
2020-12-22 04:28:40 -0500

    Functio to do nothing

---
+++ _nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Function to do nothing.
+ * @stack: head pointer.
+ * line_number: Num of the line.
+ */
+
+void _push(stack_t **stack, unsigned int line_number)
+{
+	void(stack);
+	void(line_number);
+}
