173f1318775b4f0dfc9536c38c4486bfbe17f274
Nicolas <nico15935746@gmail.com>
2020-12-22 20:38:08 -0500

    Way to free everything, changed

--- frea.c
+++ frea.c
@@ -6,8 +6,14 @@
 
 void free_all(void)
 {
+	stack_t *tmp = GBB.stack;
+
 	free(GBB.buffer);
+	while (GBB.stack)
+	{
+		tmp = GBB.stack;
+		GBB.stack = GBB.stack->next;
+		free(tmp);
+	}
 	fclose(GBB.f1);
-	if (GBB.stack)
-		free(GBB.stack);
 }
