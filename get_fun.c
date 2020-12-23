c6f8c588aaa9826d3e54831436038e9473acf5df
Nicolas <nico15935746@gmail.com>
2020-12-22 20:05:48 -0500

    Gets the proper function

---
+++ get_fun.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * get_fun - get the proper function.
+ * @line_number: Num of the line.
+ */
+
+void get_fun(unsigned int line_number)
+{
+	instruction_t ops[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL}
+	};
+	char *opcode;
+	unsigned int i = 0;
+
+	opcode = strtok(GBB.buffer, " ");
+	if (opcode)
+		for (i = 0; ops[i].opcode; i++)
+			if (strcmp(opcode, ops[i].opcode) == 0)
+			{
+				ops[i].f(&GBB.stack, line_number);
+				break;
+			}
+	if (!ops[i].opcode)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n",
+			line_number, opcode);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+}
