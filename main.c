39cca366209ba5d4e93465c8a69d2f2fca2dd570
Nicolas <nico15935746@gmail.com>
2020-12-22 20:46:15 -0500

    New line added

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	GBB.f1 = fopen(argv[1], "r");
 	if (!GBB.f1)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&GBB.buffer, &buff_sz, GBB.f1) != EOF && ++line_num)
