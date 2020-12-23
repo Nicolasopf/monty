c5fc437c61f4b33fbb0100be42d83b78a16bbb92
Nicolas <nico15935746@gmail.com>
2020-12-22 21:06:09 -0500

    Prototype for add and pop added

--- monty.h
+++ monty.h
@@ -61,5 +61,7 @@ void get_fun(unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
