2f2c24537427258872416a6f5e3d560e2b15b13c
Nicolas <nico15935746@gmail.com>
2020-12-22 20:05:28 -0500

    Push a node to the head

--- _push.c
+++ _push.c
@@ -1,13 +1,33 @@
 #include "monty.h"
 
 /**
- *
- *
+ * _push - Push to the head of a linked list.
+ * @stack: head pointer.
+ * line_number: Num of the line.
  */
 
 void _push(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
+	char *data;
+	stack_t *new;
+
+	data = strtok(NULL, " ");
+	if (isdigit(atoi(data)) != 0)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	(void)line_number;
-	printf("ASD\n");
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = atoi(data);
+	new->next = *stack;
+	if (*stack)
+		(*stack)->prev = new;
+	new->prev = NULL;
+	*stack = new;
 }
